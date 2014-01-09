614deddc1399f008680d4af94ffe89ff29dcfe09
unknown <RichardNeil@CARDING.asiapacific.hpqcorp.net>
2014-01-09 21:56:17 +0800

    Initialized grunt tasks. Updated README.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ techlifemusic-theme
 ===================
 
 Wordpress theme for tech+life+music, my homepage at http://richardneililagan.com
+
+## Using for your own development
+
+This project makes use of node modules with grunt to precompile some stuff.
+If you're interested in using this project as a base for your own theme, make sure you set up your dev env correctly:
+
+1. Make sure [Node](http://nodejs.org/) is installed on your machine.
+2. In this project's root directory, make sure that you run `npm install` (and not `npm install --production`). This will install the necessary modules required for development.
+3. The [grunt](http://gruntjs.com/getting-started) tasks for this project precompile the JS and CSS/LESS files into their respective static files. Running `grunt watch` will do this precompilation live, and any changes in your statics will result in a precompile.
+
---
+++ gruntfile.js
@@ -0,0 +1,107 @@
+module.exports = (function (contribs) {
+
+    return function (grunt) {
+
+        // initialize
+        grunt.initConfig({
+
+            pkg : grunt.file.readJSON('package.json'),
+
+            // @module jshint
+            jshint : {
+                options : {
+                    // todo
+                },
+                all : ['gruntfile.js', 'js/src/**/*.js']
+            },
+
+            // @module uglifyjs
+            uglify : {
+                options : {
+                    sourceMap : 'js/app.build.map',
+                    preserveComments : 'some',
+                    banner : '/** @author Richard Neil Ilagan [me@richardneililagan.com] */'
+                },
+                app : {
+                    files : {
+                        'js/app.min.js' : 'js/app.js'
+                    }
+                }
+            },
+
+            // @module browserify
+            browserify : {
+                main : {
+                    src : 'js/src/**/*.js',
+                    dest : 'js/app.js'
+                }
+            },
+
+            // @module LESS CSS
+            less : {
+                development : {
+                    options : {
+                        compress : false
+                    },
+                    files : {
+                        'css/style.css' : 'css/src/style.less'
+                    }
+                },
+                production : {
+                    options : {
+                        compress : true
+                    },
+                    files : {
+                        'css/style.min.css' : 'css/src/style.less'
+                    }
+                }
+            },
+
+            // @module watch
+            watch : {
+                scripts : {
+                    files : '**/*.js',
+                    tasks : ['compile-scripts'],
+                    options : {
+                        interrupt : true,
+                        spawn : false,
+                        livereload : true
+                    }
+                },
+                styles : {
+                    files : '**/*.less',
+                    tasks : ['compile-styles'],
+                    options : {
+                        interrupt : true,
+                        spawn : false,
+                        livereload : true
+                    }
+                }
+            }
+
+        });
+
+        // load grunt precompiled tasks
+        for (var i = 0; i < contribs.length; i++) {
+            grunt.loadNpmTasks(contribs[i]);
+        }
+
+        // register tasks
+        grunt.registerTask('default', 'package');
+
+        grunt.registerTask('compile-scripts', ['jshint', 'browserify']);
+        grunt.registerTask('compile-styles', ['less:development']);
+
+        grunt.registerTask('compile-scripts-production', ['compile-scripts', 'uglify']);
+        grunt.registerTask('compile-styles-production', ['less:production']);
+
+        grunt.registerTask('package', ['compile-scripts-production', 'compile-styles-production']);
+    };
+
+})([
+    'grunt-browserify',
+    'grunt-contrib-uglify',
+    'grunt-contrib-less',
+    'grunt-contrib-watch',
+    'grunt-contrib-jshint'
+]);
