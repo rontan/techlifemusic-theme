2af50b5949bd9c988e3238e40581afdf54ce8c28
Richard Neil Ilagan <richard.ilagan@hp.com>
2014-01-17 23:34:46 +0800

    More article styling: pre blocks made horizontal-scrollable

--- index.php
+++ index.php
@@ -26,12 +26,25 @@
 </header>
 
 <section id="content" role="main">
-
-    <span class="leader">latest</span>
+    
     <article>
-        <h2>Using Razor in Javascript and CSS files</h2>
-        <time datetime="2013-11-12T10:44:07+00:00">November 12, 2013</time>
+        <header>
+            <span class="leader">latest</span>
+            <h2>Using Razor in Javascript and CSS files</h2>
+            <time datetime="2013-11-12T10:44:07+00:00">November 12, 2013</time>
+        </header>
         <p>There are countless use cases where I found myself needing to use <strong>Razor</strong> syntax (in ASP.NET MVC 3+) inside Javascript, CSS and other asset files. While there are many different ways to get Razor output into external JS files (for example, getting a <code>Url.<strong>Action</strong>()</code> call into a JS file’s jQuery.ajax() call), I find that most feel like beating around the bush. After all, views in MVC don’t always have to be HTML files, at least in the traditional spirit of MVC.</p>
+        <pre>
+public class AssetController : Controller {
+    
+    public ActionResult Styles () {
+
+        Response.ContentType = “text/css”;
+        
+        var model = Context.GetEntity();
+        return View(model);
+    }
+}</pre>
     </article>    
 </section>
 
