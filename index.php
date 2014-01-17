1c4cf6c60fed21f028fe91a8690d002b4f4e9052
Richard Neil Ilagan <richard.ilagan@hp.com>
2014-01-17 23:13:01 +0800

    Ramped up the global styling to use a base font size of 18pt.

--- index.php
+++ index.php
@@ -25,7 +25,14 @@
     </section>
 </header>
 
-<div id="content" role="main">
-</div>
+<section id="content" role="main">
+
+    <span class="leader">latest</span>
+    <article>
+        <h2>Using Razor in Javascript and CSS files</h2>
+        <time datetime="2013-11-12T10:44:07+00:00">November 12, 2013</time>
+        <p>There are countless use cases where I found myself needing to use <strong>Razor</strong> syntax (in ASP.NET MVC 3+) inside Javascript, CSS and other asset files. While there are many different ways to get Razor output into external JS files (for example, getting a <code>Url.<strong>Action</strong>()</code> call into a JS file’s jQuery.ajax() call), I find that most feel like beating around the bush. After all, views in MVC don’t always have to be HTML files, at least in the traditional spirit of MVC.</p>
+    </article>    
+</section>
 
 <?php get_footer(); ?>
