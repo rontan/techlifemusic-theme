6438b1be2b0c24e1bafc035d036af99e925fd96d
Richard Neil Ilagan <richard.ilagan@hp.com>
2014-02-07 22:55:26 +0800

    Preliminary commit

--- footer.php
+++ footer.php
@@ -1,3 +1,19 @@
+<footer>
+
+    <div class="column">
+        <h5><a href="">techlifemusic</a></h5>    
+        <p>We learn something new everyday, and this serves a bit like a repository of the interesting stuff, challenges and boss fights we across in work and play.</p>
+        <a href="#">
+            <strong>Grab a copy of my resume.</strong>
+            <em>Updated: January 20, 2014</em>
+        </a>
+    </div>
+
+    <div class="column"></div>
+    <div class="column"></div>
+
+</footer>
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/lib/jquery.min.js"><\/script>')</script>
 <?php wp_footer(); ?>
--- single.php
+++ single.php
@@ -1,30 +1,5 @@
 <?php get_header(); ?>
 
-<header>
-    <h1><span>tech</span><span>life</span><span>music</span></h1>
-    <nav>
-        <ul>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Code</a></li>
-        </ul>
-    </nav>
-    <section id="loupe">
-        <div class="slide">
-            <h4>I shoot photos.<br/>I'd also like to get better at it.</h4>
-            <p>Meatloaf shank short loin, spare ribs prosciutto doner ground round sirloin. Tail shoulder pancetta t-bone. Andouille ground round shank, tri-tip flank pork chop testing yeah yeah.</p>
-        </div>
-        <div class="slide">
-            <h4>I shoot photos.<br/>I'd also like to get better at it.</h4>
-            <p>Meatloaf shank short loin, spare ribs prosciutto doner ground round sirloin. Tail shoulder pancetta t-bone. Andouille ground round shank, tri-tip flank pork chop.</p>
-        </div>
-        <div class="slide">
-            <h4>I shoot photos.<br/>I'd also like to get better at it.</h4>
-            <p>Meatloaf shank short loin, spare ribs prosciutto doner ground round sirloin. Tail shoulder pancetta t-bone. Andouille ground round shank, tri-tip flank pork chop.</p>
-        </div>
-    </section>
-</header>
-
 <section>
 
     <main id="content" role="main">
